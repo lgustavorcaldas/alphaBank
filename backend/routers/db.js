dc0d4a677c35c579be3226098a09ea1ef420c90b
Gustavo Caldas <lgustavorcaldas@gmail.ccom>
2022-06-17 18:06:05 -0300

    a

--- backend/routers/db.js
+++ backend/routers/db.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool;
 
-const client = new Pool( { connectionString: "postgres://thcfgtop:yD2dojFZunFMnAoJeq-zf3SbhtRJTs9n@motty.db.elephantsql.com/thcfgtop" } );
+const client = new Pool( { connectionString: "postgres://thcfgtop:3dRV4luvgGTOPoOzRr7V2qrqy2XzhgZk@motty.db.elephantsql.com/thcfgtop" } );
 
 client.connect( ( err ) => { if ( err ) return console.log("🚀 ~ file: transation.js ~ line 6 ~ err", err) } );
 
